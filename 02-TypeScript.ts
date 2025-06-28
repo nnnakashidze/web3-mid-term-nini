d1d4d548a674fc4d0e07671e0ac4d7023173970b
nnnakashidze <nini.nakashidze17@gmail.com>
2025-06-28 21:25:01 +0400

    midterm

--- 02-TypeScript.ts
+++ 02-TypeScript.ts
@@ -3,21 +3,26 @@
 // მაგალითი 1
 
 let name = "Alice";
+let name: string = "alice";
 
 // მაგალითი 2
 
 let numbers = [1, 2, 3];
+let numbers: number[] = [1, 2, 3];
 
 // მაგალითი 3
 
 function multiply(a, b) {
   return a * b;
 }
+function multiply(a: number, b: number): number {
+  return a * b;
+}
 
 // მაგალითი 4
 
 const user = { id: 1, name: "Alice" };
-
+const user: { id: number; name: string } = { id: 1, name: "alice" };
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
@@ -43,12 +48,21 @@ const config2 = {
 const config3 = {
   tester: "test",
 };
+type Config = {
+  theme?: string;
+  options?: {
+    fontSize?: number;
+    layout?: null;
+  };
+  tester?: string;
+};
 
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
 const array1 = [42, "hello", { name: "Alice" }];
 const array2 = ["apple", true, { isValid: false }];
 const array3 = [];
+type MixedArray = (number | string | boolean | object)[];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
@@ -58,6 +72,10 @@ type Handler = {
   validate?: (input: string) => boolean;
   log?: () => void;
 };
+process უკან აბრუნებს string-ს,
+validate აბრუნებს true და falses
+log არაფერს არ აბრუნებს.
+
 
 // მაგალითი 9
 
@@ -82,4 +100,22 @@ const user: User = {
     },
   },
   metadata: undefined,
+};
+type User = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: {
+    fullName: string;
+    age: number;
+    interests: string[];
+  };
+  settings: {
+    theme: string;
+    notifications: {
+      email: boolean;
+      sms: boolean;
+    };
+  };
+  metadata: undefined;
 };
